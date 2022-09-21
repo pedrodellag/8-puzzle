a89635a97b098fdb0b22d8c5d44650b9971ef158
mudouasenha <mudouasenha@gmail.com>
2022-09-21 19:14:18 -0300

    ajuste estrutura

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == '__main__':
+    solution_array = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+
+    solution_state = [[1, 2, 3], 
+                    [4, 5, 6], 
+                    [7, 8, 0]]
+    
+    initial_state = [[1, 2, 3], 
+                    [4, 5, 6], 
+                    [7, 0, 8]]
